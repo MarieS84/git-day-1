0db7381399513818f2db43596e2b68ca59186e50
Marie Svensson <svenssonmc84@gmail.com>
2021-06-07 13:37:56 +0200

    new method

--- src/gitday/Main.java
+++ src/gitday/Main.java
@@ -14,13 +14,14 @@ public class Main {
 
         for (int i = 0; i<10;i++){
             System.out.println(i);
+
         }
 
     }
 
     public static void method2() {
 
-        for (int i = 0; i>10;i--){
+        for (int i = 10;i>0;i--){
             System.out.println(i);
         }
 
