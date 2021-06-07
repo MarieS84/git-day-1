10138616423e5c649f751e691d224bcd4cd297fd
Marie Svensson <svenssonmc84@gmail.com>
2021-06-07 11:48:02 +0200

    changes

---
+++ src/gitday/Main.java
@@ -0,0 +1,29 @@
+package gitday;
+
+public class Main {
+
+    public static void main(String[] args) {
+        method(6);
+        System.out.println(method2());
+
+
+    }
+
+    public static void method(int a) {
+        if (a < 5) {
+            return;
+        }
+        System.out.println(a);
+
+    }
+
+    public static double method2() {
+        return 55.8;
+    }
+}
+
+
+
+
+
+
