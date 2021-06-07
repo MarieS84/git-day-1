16ba56b90e31964b5840bfc78c61c104efccd314
Marie Svensson <svenssonmc84@gmail.com>
2021-06-07 11:58:37 +0200

    changes

--- src/gitday/Main.java
+++ src/gitday/Main.java
@@ -4,21 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         method(6);
-        System.out.println(method2());
+
 
     }
 
     public static void method(int a) {
-        if (a < 5) {
-            return;
-        }
-        System.out.println(a);
+
+        System.out.println(a+1);
 
     }
 
-    public static double method2() {
-        return 55.8;
-    }
 }
 
 
